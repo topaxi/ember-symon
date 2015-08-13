897d4635122233330ef47db0c5b7c507b4a02e84
Damian Senn <damian.senn@gmail.com>
2015-08-13 20:51:17 +0200

    Responsive updates

--- app/breakpoints.js
+++ app/breakpoints.js
@@ -1,6 +1,7 @@
 export default {
   mo: '(max-width: 30rem)',
-  xs: '(min-width: 48rem)',
+  xs: '(min-width: 30rem)',
+  sm: '(min-width: 48rem)',
   md: '(min-width: 62rem)',
   lg: '(min-width: 75rem)',
   xl: '(min-width: 90rem)'

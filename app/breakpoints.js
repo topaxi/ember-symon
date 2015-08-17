c70d6b988e586eeb9ba168ba016e71d931ab0193
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-08-17 08:44:22 +0200

    Action buttons for alerts

--- app/breakpoints.js
+++ app/breakpoints.js
@@ -1,8 +1,15 @@
 export default {
-  mo: '(max-width: 30rem)',
-  xs: '(min-width: 30rem)',
-  sm: '(min-width: 48rem)',
-  md: '(min-width: 62rem)',
-  lg: '(min-width: 75rem)',
-  xl: '(min-width: 90rem)'
+  mo: '(max-width:  479px)',
+  xs: '(min-width:  480px)',
+  sm: '(min-width:  768px)',
+  md: '(min-width:  992px)',
+  lg: '(min-width: 1200px)',
+  xl: '(min-width: 1440px)',
+
+  maxMo: '(max-width:  479px)',
+  maxXs: '(max-width:  767px)',
+  maxSm: '(max-width:  991px)',
+  maxMd: '(max-width: 1199px)',
+  maxLg: '(max-width: 1439px)',
+  maxXl: '(min-width: 1440px)'
 }

169e2a22a584ca4938a8479793ab68f098a64481
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-09-02 09:35:58 +0200

    Get commands in service.new route

--- app/manage/services/new/route.js
+++ app/manage/services/new/route.js
@@ -3,5 +3,18 @@ import RouteNew from 'ember-symon/mixins/route-new'
 
 export default Ember.Route.extend(RouteNew, {
   editRoute: 'manage.services.service.edit',
-  modelName: 'service'
+  modelName: 'service',
+
+  setupController(controller, model) {
+    this.controllerFor(this.get('editRoute'))
+        .setProperties({
+          model,
+          isNew:    true,
+          commands: this.store.peekAll('command')
+        })
+  },
+
+  beforeModel() {
+    return this.store.findAll('command')
+  }
 })

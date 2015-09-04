dc82d60bc99163135eda056919fbb20593439e70
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-09-04 12:57:19 +0200

    Add / update services

--- app/manage/services/service/edit/controller.js
+++ app/manage/services/service/edit/controller.js
@@ -1,11 +1,49 @@
 import Ember from 'ember'
 
+const { inject } = Ember
+
 export default Ember.Controller.extend({
+  notify: inject.service(),
+
   actions: {
     setAlertScheduler(id) {
       this.set('model.alertScheduler',
         this.store.peekRecord('alert-scheduler', id)
       )
+    },
+
+    setCommand(id) {
+      this.set('model.command',
+        this.store.peekRecord('command', id)
+      )
+    },
+
+    addServiceArgument() {
+      this.store.createRecord('service-argument', {
+        service: this.model
+      })
+    },
+
+    rollbackServiceArgument(arg) {
+      arg.rollbackAttributes()
+    },
+
+    deleteServiceArgument(arg) {
+      arg.deleteRecord()
+    },
+
+    async save() {
+      try {
+        await this.model.save()
+        await this.model.get('arguments').map(a => a.save())
+
+        this.get('notify').success('Service successfully saved!')
+
+        this.transitionToRoute('manage.services.service.edit', this.model)
+      }
+      catch (err) {
+        this.get('notify').error(err.message || 'Error while trying to save service!')
+      }
     }
   }
 })
